3051bf2d15440c5f9f5dabdb1cdc9068d2b76320
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-10 15:52:40 -0400

    Update file

--- index.js
+++ index.js
@@ -1,10 +1,65 @@
+const cart1 = 
+  [
+    {"AVOCADO": {price: 3.0, clearance: true}},
+    {"AVOCADO": {price: 3.0, clearance: true}},
+    {"KALE": {price: 3.0, clearance: false}}
+  ]
+
 const consolidateCart = (cart) => {
-  // code here
+  const consolidatedCart = {}
+  cart.forEach((item) =>{
+    //console.log(item)
+    let currentKey = Object.keys(item)[0]
+    if(!consolidatedCart[currentKey]){
+      //console.log(Item not in consolidatedCart: ${item})
+      consolidatedCart[currentKey] = {
+        price: item[currentKey].price,
+        clearance: item[currentKey].clearance,
+        count: 1
+      }
+    }else{
+      consolidatedCart[currentKey].count++;
+    }
+  })
+  //console.log(consolidatedCart)
+  return consolidatedCart;
 }
 
-const applyCoupons = (cart, coupons) => {
-  // code here
+const cart2 = {
+  "AVOCADO": {price: 3.0, clearance: true, count: 3},
+  "KALE": {price: 3.0, clearance: false, count: 1}
 }
+
+const coupons = {item: "AVOCADO", num: 2, cost: 5.0}
+
+const applyCoupons = (cart, coupons) => {
+  let ourCart = cart;
+  // console.log(coupons)
+  let currentCouponKey = coupons.item;
+  for(const item in ourCart) {
+    // let currentCartKey = Object.keys(ourCart)[0];
+    if(item === currentCouponKey) {
+      // console.log('if ran')
+      // console.log(coupons.num)
+      // console.log(ourCart[currentCartKey].count);
+      let uncouponableItems = ourCart[item].count - coupons.num;
+      console.log(uncouponableItems)
+      ourCart[item].count = uncouponableItems
+      let clearancePrice = coupons.cost
+      ourCart[`${item} W/COUPON`] = {price: clearancePrice, clearance: true, count:1}
+    }
+  }
+  console.log(Object.keys(ourCart))
+  return ourCart;
+}
+
+console.log(applyCoupons(cart2, coupons));
+
+// {
+//   "AVOCADO": {price: 3.0, clearance: true, count: 1},
+//   "KALE": {price: 3.0, clearance: false, count: 1},
+//   "AVOCADO W/COUPON": {price: 5.0, clearance: true, count: 1}
+// }
 
 const applyClearance = (cart) =>{
   // code here
