a7c789097cdddecd472d668d97a8c17791437f21
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-13 14:09:45 -0400

    Refactor checkout function

--- index.js
+++ index.js
@@ -15,39 +15,26 @@ const consolidateCart = (items) => {
   return cart
 }
 
-const cart2 = {
-  "AVOCADO": {price: 3.0, clearance: true, count: 2},
-  "KALE": {price: 3.0, clearance: false, count: 1}
-}
-
-const coupons = [{item: "AVOCADO", num: 2, cost: 5.0}]
-
 const applyCoupons = (cart, coupon) => {
-  if(coupon.length > 0) {
-  const couponProduct = coupon[0].item
-  for(const item in cart){
-    if(item === couponProduct){
-      if(cart[item].count - coupon[0].num >= 0){
-        const eligibleSetsForCoupon = Math.floor(cart[item].count / coupon[0].num)
-        cart[`${item} W/COUPON`] = {
-          price: coupon[0].cost,
-          clearance: cart[item].clearance,
-          count: eligibleSetsForCoupon
+  coupon.forEach(coupon => {
+    const couponProduct = coupon.item
+    for(const item in cart){
+      if(item === couponProduct){
+        if(cart[item].count - coupon.num >= 0){
+          const eligibleSetsForCoupon = Math.floor(cart[item].count / coupon.num)
+          cart[`${item} W/COUPON`] = {
+            price: coupon.cost,
+            clearance: cart[item].clearance,
+            count: eligibleSetsForCoupon
+          }
+          const remainingCount =  cart[item].count - (eligibleSetsForCoupon * coupon.num)
+          cart[item].count = remainingCount
         }
-        const remainingCount =  cart[item].count - (eligibleSetsForCoupon * coupon[0].num)
-        cart[item].count = remainingCount
       }
-    }
+      return cart
+      }
+    })
     return cart
-  }
-  }
-  return cart
-}
-
-const cart3 = {
-  "PEANUTBUTTER": {price: 3.00, clearance: true, count: 2},
-  "KALE": {price: 3.00, clearance: false, count: 3},
-  "SOY MILK": {price: 4.50, clearance: true, count: 1}
 }
 
 const applyClearance = (cart) =>{
@@ -65,10 +52,14 @@ const checkout = (cart, coupons) => {
   const ourCart = consolidateCart(cart)
   const couponedCart = applyCoupons(ourCart, coupons)
   const discountCart = applyClearance(couponedCart);
-  console.log(discountCart)
   let cartTotal = 0
   for(let item in discountCart) {
     cartTotal += discountCart[item].price * discountCart[item].count
   } 
+  if(cartTotal > 100) {
+    cartTotal = cartTotal * (1 - .10)
+    cartTotal = Math.round(cartTotal * 100) / 100
+    return cartTotal
+  }
 return cartTotal
 }
